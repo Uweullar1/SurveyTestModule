384f2cb7843ee7a674f4d518db1c09fbbde4b72d
Uweullar <uweullar@gmail.com>
2026-04-23 19:36:39 +0300

    Ошибка белого экрана при загрузке

--- opros-module/src/router/index.js
+++ opros-module/src/router/index.js
@@ -1,5 +1,5 @@
 // src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { supabase } from '../supabase'
 
 
