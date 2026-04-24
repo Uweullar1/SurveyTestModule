7da15225a5a537f9b2deeb38c62929274352fc1b
Uweullar <uweullar@gmail.com>
2026-04-24 19:59:47 +0300

    перемещение supabase-proxy

--- opros-module/src/supabase.js
+++ opros-module/src/supabase.js
@@ -1,7 +1,7 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = window.location.origin + '/api/supabase-proxy'
+const supabaseUrl = window.location.origin + '/supabase-proxy'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvamFzY3B3Y2t2aWtkcWxiZnZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MTYxNDksImV4cCI6MjA4ODQ5MjE0OX0.M6Se9csIMpeAvOBJzi2VH1VY1scAOtaE44HZ_FWUbAo'  // вставь свой реальный ключ из Supabase → Settings → API
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
