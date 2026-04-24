f86b8a20a914df0dbc263e231bb1db7c9286476e
Uweullar <uweullar@gmail.com>
2026-04-24 22:30:09 +0300

    Проблема потери данных с бд

--- opros-module/vite.config.js
+++ opros-module/vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        }
+    },
     server: {
         proxy: {
             '/api/supabase-proxy': {
-                target: 'https://vojascpwckvikdqlbfvy.supabase.co/', // Добавь слеш в конце
+                target: 'https://vojascpwckvikdqlbfvy.supabase.co',
                 changeOrigin: true,
                 secure: false,
                 rewrite: (path) => path.replace(/^\/api\/supabase-proxy/, ''),
-                // Это оставит всё, что идет ПОСЛЕ префикса (например, /auth/v1/signup)
             }
         }
-    
     }
 })
