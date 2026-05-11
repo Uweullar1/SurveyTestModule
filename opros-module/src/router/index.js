847d2dfd727efa0f5fe2762e402e6e4630c6e444
Uweullar <uweullar@gmail.com>
2026-05-11 04:06:37 +0300

    1

--- opros-module/src/router/index.js
+++ opros-module/src/router/index.js
@@ -61,11 +61,6 @@ const routes = [
         name: 'Admin',
         component: () => import('../views/AdminView.vue'),
         meta: { requiresAuth: true }
-    },
-    {
-        path: '/reset-password',
-        name: 'ResetPassword',
-        component: () => import('../views/ResetPasswordView.vue')
     }
 
 ]
